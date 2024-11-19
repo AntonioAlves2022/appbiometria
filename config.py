c67eb871b50810e0f78521be76319c348372c28d
Antonio Alves <antonio.alves@duque.g12.br>
2024-11-19 10:57:54 -0300

    Database setup

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:231281@localhost:3306/biometria'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://professor:iDfH9ycUjexAPX6nhWYxGSvvtboq3y16@dpg-cstp5jjtq21c73bpbsig-a.oregon-postgres.render.com/faceid'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:TBNOywoFaDKYPffcvvzcAmFWTiOQOaHv@postgres.railway.internal:5432/railway'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'uploads'
     SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
