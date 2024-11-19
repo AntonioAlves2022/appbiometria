e63267c6db8e5297020f8f55b2ac298b3dba00a0
Antonio Alves <antonio.alves@duque.g12.br>
2024-11-19 16:14:12 -0300

    Setup Heroku DB Postgresql

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:231281@localhost:3306/biometria'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'default_local_db_url')
+    SQLALCHEMY_DATABASE_URI = 'postgres://ue56gacpg836tn:p67660c244347c8e12a5ac3dc56799125e2970fea3a952b4d464f79c0755dcd15@ccpa7stkruda3o.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d2980vnprjccv'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'uploads'
     SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
